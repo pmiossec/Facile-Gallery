a5bbdf60a2b84192c3fb4f480970f627946f56c2
Philippe Miossec <pmiossec@gmail.com>
2010-11-02 20:20:10 +0100

    translation permitted Second part... :)

---
+++ fr.php
@@ -0,0 +1,13 @@
+<?php
+/***********************************************
+ *******************Translation*****************
+ **********************************************/
+define('PHOTO_DIR_NEEDED','Vous devez sp&eacute;cifier un r&eacute;pertoire photo !');
+define('PHOTO_DIR_NOT_EXISTING','Ce r&eacute;pertoire photo n\'existe pas !');
+define('DISPLAY_MAP','Afficher la carte');
+define('NO_PHOTO_TO_DISPLAY','Il n\'y a aucune photo à afficher !');
+define('NO_PHOTO_WITH_GPS_DATA','Aucune photo ne contient de données GPS :(');
+define('SLIDESHOW','Diaporama');
+define('OPEN_IN_GOOGLE_MAP','Ouvrir sur google map');
+define('TAGS','Tags : ');
+?>
